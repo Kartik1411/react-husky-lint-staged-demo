739e5fcdc5b3b17fd07e15d802ac9cfec6e9bc8a
Kartik <kartikvarshney1411@gmail.com>
2022-02-02 13:39:11 +0530

    app.js changes

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -15,11 +15,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Demo for eslint, prettier with eslint, husky and lint-staged
         </a>
       </header>
     </div>
   );
 }
-
+debugger;
 export default App;
